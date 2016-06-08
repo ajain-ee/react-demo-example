6c59c610fbd17dfbdb5780115a191a0d204bc15e
anuj-jain <anuj.jain@digital.hmrc.gov.uk>
2016-06-08 15:31:18 +0530

    added example for react-router implementation

---
+++ react-router/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class App extends React.Component {
+    render() {
+      return (
+         <div>
+            <ul>
+               <li><Link to="/">Home</Link></li>
+               <li><Link to="/about">About</Link></li>
+               <li><Link to="/contact">Contact</Link></li>
+            </ul>
+            
+           {this.props.children}
+         </div>
+      )
+   }
+}
+
+class Home extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Home...</h1>
+         </div>
+      )
+   }
+}
+
+class About extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>About...</h1>
+         </div>
+      )
+   }
+}
+
+class Contact extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Contact...</h1>
+         </div>
+      )
+   }
+}
+
+export {Home, Contact, About, App}
---
+++ react-router/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App, Home, Contact,About} from './App.jsx';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router'
+
+
+
+ReactDOM.render((
+   <Router history = {browserHistory}>
+      <Route path = "/" component = {App}>
+         <IndexRoute component = {Home} />
+         <Route path = "home" component = {Home} />
+         <Route path = "about" component = {About} />
+         <Route path = "contact" component = {Contact} />
+      </Route>
+   </Router>
+	
+), document.getElementById('app'))
